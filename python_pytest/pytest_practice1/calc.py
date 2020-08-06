1d37bcc15bd3175596b47262c9c0fb1488a47a29
twinklecjj <2312526945@qq.com>
2020-08-07 07:46:41 +0800

    python pytest测试实战1

---
+++ python_pytest/pytest_practice1/calc.py
@@ -0,0 +1,18 @@
+# -*-coding:utf-8-*-
+# 计算器的代码
+class Calculator:
+    # 加+
+    def add(self, a, b):
+        return a + b
+
+    # 减-
+    def sub(self, a, b):
+        return a - b
+
+    # 乘*
+    def mul(self, a, b):
+        return a * b
+
+    # 除÷
+    def div(self, a, b):
+        return a / b
